8b186bcc929bdf47f60f97322960516203e7108c
Bertrand <bertrand.marlair@gmail.com>
2025-05-14 10:53:53 +0200

    Update Docker image tags in docker-compose and pull script
    - Change the image tag for the stella-runner service from 'latest-test' to 'latest' in docker-compose-server.yml.
    - Modify the pull.sh script to correctly pull the latest images for stella-vslam and stella-runner, ensuring proper syntax for the docker pull commands.

--- pull.sh
+++ pull.sh
@@ -1,4 +1,4 @@
 aws ecr get-login-password --region eu-north-1 | docker login --username AWS --password-stdin 381492108418.dkr.ecr.eu-north-1.amazonaws.com
-docker pull 381492108418.dkr.ecr.eu-north-1.amazonaws.com/bimefy/insv-processor:latest
-docker pull 381492108418.dkr.ecr.eu-north-1.amazonaws.com/bimefy/stella-vslam:latest
 docker pull 381492108418.dkr.ecr.eu-north-1.amazonaws.com/bimefy/stella-vslam-viewer:latest
+docker 381492108418.dkr.ecr.eu-north-1.amazonaws.com/bimefy/stella-vslam:latest
+docker 381492108418.dkr.ecr.eu-north-1.amazonaws.com/bimefy/stella-runner:latest
